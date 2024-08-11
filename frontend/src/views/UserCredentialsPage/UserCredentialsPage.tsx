aa11ac9347b86a0cf40f036b1cdd21132bb99975
GLEF1X <hlibharanin.work@gmail.com>
2024-08-11 10:28:47 -0400

    fix(user-credentials): change meta page title to `User secrets`

--- frontend/src/views/UserCredentialsPage/UserCredentialsPage.tsx
+++ frontend/src/views/UserCredentialsPage/UserCredentialsPage.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Link from "next/link";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,6 +8,11 @@ import { UserCredentialsSection } from "./components/UserCredentialsSection";
 export const UserCredentialsPage = () => {
   return (
     <div className="container mx-auto h-full w-full max-w-7xl bg-bunker-800 px-6 text-white">
+      <Head>
+        <title>User secrets</title>
+        <link rel="icon" href="/infisical.ico" />
+        <meta property="og:image" content="/images/message.png" />
+      </Head>
       <div className="flex items-center justify-between py-6">
         <div className="flex w-full flex-col">
           <h2 className="text-3xl font-semibold text-gray-200">User Secrets</h2>
